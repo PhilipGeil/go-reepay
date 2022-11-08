1168f2bcea8b3a9ab788b22f43f1c24af86a2dde
Philip Geil Jensen <pgj.philip@gmail.com>
2022-11-08 09:39:51 +0100

    Added .env file for variables

--- example/main.go
+++ example/main.go
@@ -3,20 +3,29 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	go_reepay "go-reepay"
+	"log"
+	"os"
 )
 
-var Reepay = go_reepay.Reepay{
-	Key:        "priv_8ed48c74022948202c43bbe22fdf3a20",
-	SuccessURL: "https://test.dk",
-	CancelURL:  "https://test.dk",
-}
+var Reepay go_reepay.Reepay
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	Reepay = go_reepay.Reepay{
+		Key:        os.Getenv("REEPAY_PRIV_KEY"),
+		SuccessURL: os.Getenv("ACCEPT_URL"),
+		CancelURL:  os.Getenv("CANCEL_URL"),
+	}
 	router := gin.Default()
 
 	router.POST("/charge", CreateCharge)
-	err := router.Run(":8080")
+	router.POST("/settle", SettleCharge)
+	err = router.Run(":8080")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -30,6 +39,18 @@ func CreateCharge(c *gin.Context) {
 		return
 	}
 	err := CreateChargeSession(c, charge)
+	if err != nil {
+		return
+	}
+}
+
+func SettleCharge(c *gin.Context) {
+	var charge go_reepay.ChargeDTO
+	if err := c.BindJSON(&charge); err != nil {
+		fmt.Println(err)
+		return
+	}
+	err := SettleSession(c, charge)
 	if err != nil {
 		return
 	}
